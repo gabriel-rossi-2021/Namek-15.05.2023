b091f75213c5d6294650471b17f495de9fcd049e
gabriel-rossi-2021 <81309938+gabriel-rossi-2021@users.noreply.github.com>
2023-05-15 17:46:03 +0200

    Update 2023_02_16_140143_create_order_table.php

--- database/migrations/2023_02_16_140143_create_order_table.php
+++ database/migrations/2023_02_16_140143_create_order_table.php
@@ -19,9 +19,6 @@ return new class extends Migration
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id_users')->on('users')->onDelete('cascade');
 
-            $table->unsignedBigInteger('product_id');
-            $table->foreign('product_id')->references('id_product')->on('products')->onDelete('cascade');
-
             // Date / Heure
             $table->timestamps();
 
